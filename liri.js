98d155e55813c37fd03cfcd4e5089587da12a546
Herculano Ramos <herc@Herculanos-MBP.fios-router.home>
2018-12-03 13:39:09 -0500

    trying to make bandsintown work

--- liri.js
+++ liri.js
@@ -39,8 +39,6 @@ function doTheSpotify() {
     if (err) {
       console.log("Error occured: " + err);
     } else {
-      // Returns JSON info for selected track
-      // console.log(JSON.stringify(data, null, 2));
       console.log("{{{**********||**********}}}");
       console.log(
         "\nArtist: " +
@@ -60,22 +58,26 @@ function doTheSpotify() {
 
 function doTheBandThing() {
   console.log("Doing the band thing");
+  var queryUrl =
+    "https://rest.bandsintown.com/artists/" +
+    value +
+    "/events?app_id=codingbootcamp";
+
+  request(queryUrl, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      console.log("{{{**********||**********}}}");
+
+      console.log("{{{**********||**********}}}");
+    }
+  });
 }
 
 function theMovieThing() {
-  // Grab the movieName which will always be the third node argument.
-
-  // Then run a request to the OMDB API with the movie specified
   var queryUrl =
     "http://www.omdbapi.com/?t=" + value + "&y=&plot=short&apikey=trilogy";
 
-  // This line is just to help us debug against the actual URL.
-
   request(queryUrl, function(error, response, body) {
-    // If the request is successful
     if (!error && response.statusCode === 200) {
-      // Parse the body of the site and recover just the imdbRating
-      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
       console.log("{{{**********||**********}}}");
       console.log("Movie Title: " + JSON.parse(body).Title);
       console.log("Release Year: " + JSON.parse(body).Year);
